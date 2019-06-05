75d583d5294189cf52486832494b4ff83cfffe61
Kujanova <denisa.kujanova@kujanova-denisa-wifi.kancelar.seznam.cz>
2019-06-05 13:37:17 +0200

    First commit

---
+++ app/src/main/java/com/denisa/bookapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.denisa.bookapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.denisa.bookapp.login.LoginFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.mainContent, LoginFragment())
+            .commit()
+    }
+}
